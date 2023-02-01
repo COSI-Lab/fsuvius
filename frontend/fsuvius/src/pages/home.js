a3f32e9a036900bf5ece8df73e19c72aa217af62
Juno <jvmeifert@gmail.com>
2023-02-01 18:11:15 -0500

    Add marquee

--- frontend/fsuvius/src/pages/home.js
+++ frontend/fsuvius/src/pages/home.js
@@ -16,6 +16,11 @@ class Home extends Component {
           <div className="spacer"></div>
             <div className="content_outer box">
               <div className="content_inner">
+                <div className="marquee_container">
+                  <div className="marquee">
+                    <b>Clarkson Open Source Institute - Mount Fsuvius - Floating Soda Unit Exchange</b>
+                  </div>
+                </div>
                 <div className="centered_container">
                   <img src={FsuviusLogo} alt="Mount Fsuvius" width="100%"></img>
                 </div>
