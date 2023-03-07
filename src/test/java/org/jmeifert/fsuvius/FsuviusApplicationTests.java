7f29dd4ae5711a42d643cf85b2f77337350e5984
Juno <jvmeifert@gmail.com>
2023-03-07 10:23:29 -0500

    start experimenting with unit tests

--- src/test/java/org/jmeifert/fsuvius/FsuviusApplicationTests.java
+++ src/test/java/org/jmeifert/fsuvius/FsuviusApplicationTests.java
@@ -3,11 +3,22 @@ package org.jmeifert.fsuvius;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.jmeifert.fsuvius.user.User;
+
 @SpringBootTest
 class FsuviusApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+	void testUserInstantiation() {
+		System.out.println("===== Testing user instantiation: =====");
 
+		System.out.println("Default constructor: ");
+		System.out.println(new User());
+
+		System.out.println("With name \"joe\":");
+		System.out.println(new User("joe"));
+
+		System.out.println("With name \"joe\", balance 4.0:");
+		System.out.println(new User("joe", 4.0F));
+	}
 }
