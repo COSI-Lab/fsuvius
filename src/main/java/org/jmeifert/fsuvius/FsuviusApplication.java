4847c0c82a6fdd9b2175ebc7d3ca5d8eaa4f6985
Juno <jvmeifert@gmail.com>
2023-05-02 11:42:15 -0400

    add missing comments for javadoc

--- src/main/java/org/jmeifert/fsuvius/FsuviusApplication.java
+++ src/main/java/org/jmeifert/fsuvius/FsuviusApplication.java
@@ -3,8 +3,15 @@ package org.jmeifert.fsuvius;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * FsuviusApplication tells Spring how to configure and start the application.
+ */
 @SpringBootApplication
 public class FsuviusApplication {
+	/**
+	 * Starts the application.
+	 * @param args Unused as of now
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(FsuviusApplication.class, args);
 	}
