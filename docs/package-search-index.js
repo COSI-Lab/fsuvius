8c86d0b00ab1a83b6bcbf54cd339c10ff27155a3
Juno <jvmeifert@gmail.com>
2023-09-13 19:15:24 -0400

    generate javadoc

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.jmeifert.fsuvius"},{"l":"org.jmeifert.fsuvius.error"},{"l":"org.jmeifert.fsuvius.user"},{"l":"org.jmeifert.fsuvius.util"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.jmeifert.fsuvius"},{"l":"org.jmeifert.fsuvius.data"},{"l":"org.jmeifert.fsuvius.error"},{"l":"org.jmeifert.fsuvius.user"},{"l":"org.jmeifert.fsuvius.util"}];updateSearchResults();
