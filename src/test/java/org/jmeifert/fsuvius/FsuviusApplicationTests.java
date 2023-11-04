e57210c38bf5965e73975c5dc329e299256b77cd
Juno <jvmeifert@gmail.com>
2023-11-04 11:02:11 -0400

    remove mock test

--- src/test/java/org/jmeifert/fsuvius/FsuviusApplicationTests.java
+++ src/test/java/org/jmeifert/fsuvius/FsuviusApplicationTests.java
@@ -1,25 +1,13 @@
 package org.jmeifert.fsuvius;
 
-import org.jmeifert.fsuvius.util.Log;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.io.IOException;
-
 
 @SpringBootTest
 class FsuviusApplicationTests {
 
 	@Test
-	void testFsuviusController() throws IOException {
-		Log log = new Log("FsuviusApplicationTests");
-		log.print("Starting test of FsuviusController...");
-
-		log.print("Creating a FsuviusController...");
-		FsuviusController fc = new FsuviusController();
-
-		log.print("Test of FsuviusController completed.");
-	}
+	void testFsuviusController() {}
 
 	// TODO write proper tests
 }
